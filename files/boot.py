9d43cdd1dedd62c14c77af8012a518cc9b5dc5f0
Johan Guldmyr <martbhell@users.noreply.github.com>
2018-09-20 12:16:29 +0300

    Use a memtest.0 to optionally boot into a memtest instead of kickstart (#10)
    This has been tested and it boots a Dell R640 and R740 BIOS (not UEFI) with an memtest86+ v5.01 from 2015.
    
    Controlled by touching a file in memtest86/(mynodes short hostname). The file is removed before booting into memtest which autostarts. After memtest is done the server is waiting for a keystroke (escape) to quit and resume booting.
    
    memtest86 has preference over reinstall (if both files exist the server runs a memtest86). On next boot it will reinstall.
    
    https://www.memtest86.com/features.htm says one has to get the "site edition" of memtest86 for the network (PXE) boot feature.
    
    Some background details about what we are booting (and what needs to be copied onto a web server which the nodes can reach via http within the iPXE script):
    
    According to a forum post by an mcb30 Lead Developer of iPXE memtest.0 is:
    
    " This is memtest in the form of a PXE NBP: it has the advantage that you can return to iPXE afterwards (and report the test result, and potentially capture a screenshot of the memtest output and send it to a reporting server via HTTP POST)."
    
    I checked in version 7.5 of memtest86+ src.tgz from the ISO and it does
    not contain "pxe" anywhere in the code. It is possible to make a
    similar patch to 7.5's Makefile and add the pxe.S file to build a memtest.0 but with that build the server did not manage to boot into memtest. Maybe it would work if one took in more of the patches from https://git.ipxe.org/people/mcb30/memtest.git/shortlog/refs/heads/pxe501

--- files/boot.py
+++ files/boot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+""" Prints an iPXE script to stdout to start a kickstart & more """
 import syslog
 import sys
 import os
@@ -10,16 +11,18 @@ print
 syslog.openlog("boot.py")
 syslog.setlogmask(syslog.LOG_UPTO(syslog.LOG_INFO))
 
-try:
-    # from the name, e.g. c1-3 take c1-3
-    hostname = socket.gethostbyaddr(os.environ["REMOTE_ADDR"])[0].split(".")[0]
-    syslog.syslog(syslog.LOG_DEBUG, "Got boot iPXE request from " + hostname)
+# from the name, e.g. c1-3.cloud.example.org take c1-3
+hostname = socket.gethostbyaddr(os.environ["REMOTE_ADDR"])[0].split(".")[0]
+syslog.syslog(syslog.LOG_DEBUG, "Got boot iPXE request from " + hostname)
 
-    os.stat("/var/www/provision/reinstall/" + hostname)
-    os.remove("/var/www/provision/reinstall/" + hostname)
+started = False
+# When a hypervisor restarts without a reinstall/memtest it is expected that the tries fails. 
+try:
+    os.stat("/var/www/provision/memtest86/" + hostname)
+    os.remove("/var/www/provision/memtest86/" + hostname)
     f = open("/var/www/provision/nodes/" + hostname + ".conf")
 
-    syslog.syslog(syslog.LOG_INFO, "Reinstalling node " + hostname)
+    syslog.syslog(syslog.LOG_INFO, "Memtesting node " + hostname)
     nodesettings = {}
     for line in f.readlines():
       #for every line, e.g. "key=value", set nodesettings["key"]="value"
@@ -30,18 +33,53 @@ try:
             pass
 
     f.close()
+    # http://forum.ipxe.org/showthread.php?tid=7937&highlight=memtest
+    # https://git.ipxe.org/people/mcb30/memtest.git/commitdiff/fac651cb5f52f4dc2435c5c11ada06215a5b9ec9
+    # there is no "pxe" referenced in memtest86+ 7.5 source code (found inside the iso)
     print "#!ipxe"
-    print "kernel " + nodesettings["kernel_url_path"] + "/vmlinuz ks=" + nodesettings["kickstart_url"] + " edd=off ksdevice=bootif kssendmac console=ttyS1,115200 console=tty0 initrd=initrd.img " + nodesettings.get("extra_kernel_params", "")
-    print "initrd " + nodesettings["kernel_url_path"] + "/initrd.img"
+    print "kernel " + nodesettings['memtest86_0_path']
     print "boot"
+    started = True
 
-# Catch the exception when the reinstall file wasn't found
+# Catch the exception when the memtest file wasn't found
 except OSError:
-    print "#!ipxe"
-    print "exit"
+    pass
 # Catch all other problems
-except Exception as e:
-    print str(e)
-    syslog.syslog(syslog.LOG_ERR, str(e))
+except Exception as exc:
+    #print str(exc)
+    syslog.syslog(syslog.LOG_ERR, str(exc))
+
+########
+
+if started == False:
+    try:
+        os.stat("/var/www/provision/reinstall/" + hostname)
+        os.remove("/var/www/provision/reinstall/" + hostname)
+        f = open("/var/www/provision/nodes/" + hostname + ".conf")
+
+        syslog.syslog(syslog.LOG_INFO, "Reinstalling node " + hostname)
+        nodesettings = {}
+        for line in f.readlines():
+          #for every line, e.g. "key=value", set nodesettings["key"]="value"
+          #comment lines will throw an error, skip them
+            try:
+                nodesettings[line.split("=")[0]] = line.split("=", 1)[1].strip()
+            except:
+                pass
+
+        f.close()
+        print "#!ipxe"
+        print "kernel " + nodesettings["kernel_url_path"] + "/vmlinuz ks=" + nodesettings["kickstart_url"] + " edd=off ksdevice=bootif kssendmac console=ttyS1,115200 console=tty0 initrd=initrd.img " + nodesettings.get("extra_kernel_params", "")
+        print "initrd " + nodesettings["kernel_url_path"] + "/initrd.img"
+        print "boot"
+
+    # Catch the exception when the any memtest/reinstall file wasn't found
+    except OSError:
+        print "#!ipxe"
+        print "exit"
+    # Catch all other problems
+    except Exception as exc:
+        print str(exc)
+        syslog.syslog(syslog.LOG_ERR, str(exc))
 
 syslog.closelog()
