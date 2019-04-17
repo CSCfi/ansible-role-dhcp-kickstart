22bdeaa7958a66db5b1ddc59db56ee6cb5704601
Johan Guldmyr <johan.guldmyr@csc.fi>
2019-04-17 06:38:57 +0300

    Remove whitespaces/pylint 10/10

--- tests/test_json.py
+++ tests/test_json.py
@@ -2,7 +2,7 @@
 
 import json
 
-with open('/var/www/provsion/nodes/pxe_nodes.json') as json_file:                 
-   data = json.load(json_file)     
-   # If we can't load it it's not valid according to the python json library
-   # For example is a comma is missing somewhere python will giev a ValueError when loading it
+with open('/var/www/provision/nodes/pxe_nodes.json') as json_file:
+    DATA = json.load(json_file)
+    # If we can't load it it's not valid according to the python json library
+    # For example is a comma is missing somewhere python will giev a ValueError when loading it
